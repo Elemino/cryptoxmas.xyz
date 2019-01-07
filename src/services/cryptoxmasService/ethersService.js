9b6780ecbc6d85cd95269ad6eb9a752149f07abe
Elemino <elemshalit@gmail.com>
2019-01-07 04:37:17 -0800

    Add wallet and ethers min file to debug in browser

--- src/services/cryptoxmasService/ethersService.js
+++ src/services/cryptoxmasService/ethersService.js
@@ -1,5 +1,6 @@
 import { ethers, providers } from "ethers";
 
+
 const ethersService = () => {
 // Connect to the network
 // Connect to the Contract using a Provider, so we only have read-only access to the contract
@@ -8,9 +9,14 @@ const getBalance = async (options) => {
 const options = {
    address: '0xcBD901dB55c9139828f7b5D5Cbfd5AfeAB01d066"', 
    provider: providers.getDefaultProvider(network), 
-   const abi;  
+   const abi;
+   const wallet = new ethers.Wallet(privateKey, provider);  
  }
-let contract = new ethers.Contract(address, cardId, abi, provider);
+
+let walletWithoutProvider = await ethers.Wallet.fromEncryptedJson(JSON.stringify(encryptedWallet), password);
+let walletWithProvider = walletWithoutProvider.connect(provider);
+
+const contract = new ethers.Contract(address, cardId, abi, wallet);
 const balance = await contract.getBalance(address);
 return balance.toString();
 
